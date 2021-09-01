e5faf323e3c7fa05c6fb86638a269966b9253b98
torvic <130111@unsaac.edu.pe>
2021-09-01 11:46:15 -0500

    Feat(project): deploy my project in Vue

---
+++ src/main.js
@@ -0,0 +1,20 @@
+import Vue from 'vue'
+import App from './App.vue'
+import '@/assets/css/tailwind.css'
+import Chartkick from 'vue-chartkick'
+import { VueSpinners } from '@saeris/vue-spinners'
+import Chart from 'chart.js'
+
+import router from '@/router'
+import { dollarFilter, percentFilter } from '@/filters'
+
+Vue.use(VueSpinners)
+Vue.use(Chartkick.use(Chart))
+Vue.filter('dollar', dollarFilter)
+Vue.filter('percent', percentFilter)
+Vue.config.productionTip = false
+
+new Vue({
+  router,
+  render: (h) => h(App),
+}).$mount('#app')
